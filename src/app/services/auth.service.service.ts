39970ae61d5ec420185360451386f1d06761efbd
bdaluz <143043482+bdaluz@users.noreply.github.com>
2025-07-25 23:34:10 -0300

    Initial project setup
    This commit sets up the basic Angular project structure and integrates it with the backend API.
    It includes:
    - Project creation using Angular CLI.
    - Basic routing configuration
    - Integration points for the backend API.

---
+++ src/app/services/auth.service.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthServiceService {
+  baseApiUrl: string = environment.baseApiUrl;
+  constructor(private http: HttpClient) {}
+
+  signup(username: string, email: string, password: string): Observable<any> {
+    return this.http.post(`${this.baseApiUrl}User/CreateUser`, {
+      username,
+      email,
+      password,
+    });
+  }
+}
